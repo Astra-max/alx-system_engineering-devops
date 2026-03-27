6a77996ab6007ed53b1bde729488041d7b03aa85
Astra-max <waoremaxwel17@gmail.com>
2026-03-27 22:08:18 +0300

    managing several containers with docker compose

---
+++ 0x04-docker_containers/docker-advanced/docker-compose.sh
@@ -0,0 +1,27 @@
+# compose manage and run several container
+
+# with docker compose it ctreate a common network from which containers
+# communicates from
+
+# docker compose yaml file structure
+
+# version -- holds the version of docker comppose
+        # services -- the containers we want to create
+                  # image -- docker image container will be launched from
+                  # port -- mapping host port to containers port
+                  # environment -- list of env variables required by container
+
+
+# version: '3'
+# mongodb:
+     # image: mongo
+    # ports:
+      # -27017:27017
+    # environment:
+      # - MONGO_INITDB_ROOT_USERNAME=astra 
+# mongo-express:
+    # image: momgo-express
+    # ports:
+    # -8081:8081
+    # environment:
+    # - ME_CONFIG_MONGODB_ADMIN_USERNAME=admin
